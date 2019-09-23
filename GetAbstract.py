cab84221764271ef0c07ea7b32370e9eb17c640f
Claudio Scheer <claudioscheer@protonmail.com>
2019-09-23 20:42:54 -0300

    Format python files and update output messages

--- GetAbstract.py
+++ GetAbstract.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, './pybtex/')
+sys.path.insert(0, "./pybtex/")
 from pybtex.database import parse_file
 from pybtex.database import BibliographyData, Entry
 
@@ -13,7 +13,7 @@ import re
 import argparse
 
 
-#=============================================================
+# =============================================================
 def run(database, bibFileName, proxy, limit):
 
     print("=========================================")
@@ -21,7 +21,7 @@ def run(database, bibFileName, proxy, limit):
     print("=========================================")
 
     if not os.path.isfile(bibFileName):
-        print("File not found: ",bibFileName)
+        print("File not found: ", bibFileName)
         return
 
     bibData = parse_file(bibFileName)
@@ -34,131 +34,136 @@ def run(database, bibFileName, proxy, limit):
 
     for entry in bibData.entries.values():
         processed = processed + 1
-        
-        print(processed, entry.key+": ", end="", flush=True)
+
+        print(processed, entry.key + ": ", end="", flush=True)
 
         if "abstract" in entry.fields.keys():
             hadAbstract = hadAbstract + 1
             print("had abstract")
 
-        elif database=="acm" and not 'acmid' in entry.fields.keys():
+        elif database == "acm" and not "acmid" in entry.fields.keys():
             withoutAcmId = withoutAcmId + 1
 
         else:
 
-            if database=="springer":
-                site = entry.fields['url']
+            if database == "springer":
+                site = entry.fields["url"]
                 if site.find("chapter") != -1:
                     site = "http://link.springer.com/chapter/"
                 else:
                     site = "http://link.springer.com/article/"
-                
-                site = site+entry.key
-            
-            elif database=="acm":
-                site = "https://dl.acm.org/tab_abstract.cfm?id="+entry.fields['acmid']
 
-            elif database=="ieee":
-                site = "https://ieeexplore.ieee.org/document/"+entry.key+"/"
-                
-            
+                site = site + entry.key
+
+            elif database == "acm":
+                site = "https://dl.acm.org/tab_abstract.cfm?id=" + entry.fields["acmid"]
+
+            elif database == "ieee":
+                site = "https://ieeexplore.ieee.org/document/" + entry.key + "/"
+
             try:
-                if not proxy==None: 
-                    #use Proxy
-                    proxy_handler=urllib.request.ProxyHandler({"https":proxy})
+                if not proxy == None:
+                    # use Proxy
+                    proxy_handler = urllib.request.ProxyHandler({"https": proxy})
                     opener = urllib.request.build_opener(proxy_handler)
                     urllib.request.install_opener(opener)
 
-                req = Request(site, headers={'User-Agent': 'Mozilla/5.0'})
+                req = Request(site, headers={"User-Agent": "Mozilla/5.0"})
                 html = urlopen(req).read()
                 htmlText = html.decode("utf-8")
 
-                if database=="springer":
-                    inicio = htmlText.find('<strong class="EmphasisTypeBold ">Abstract.</strong>')
+                if database == "springer":
+                    inicio = htmlText.find(
+                        '<strong class="EmphasisTypeBold ">Abstract.</strong>'
+                    )
                     if inicio == -1:
                         inicio = htmlText.find('<h2 class="Heading">Abstract</h2>')
                     if inicio == -1:
                         inicio = htmlText.find('<h2 class="Heading">Abstract.</h2>')
                     if inicio == -1:
-                        inicio = htmlText.find('Abstract')
+                        inicio = htmlText.find("Abstract")
 
                     htmlText = htmlText[inicio:]
 
-                    inicio =  htmlText.find('<p ')
-                    if inicio>-1 and inicio<50:
+                    inicio = htmlText.find("<p ")
+                    if inicio > -1 and inicio < 50:
                         htmlText = htmlText[inicio:]
 
-                    final = htmlText.find('</section>')
+                    final = htmlText.find("</section>")
                     subhtmlText = htmlText[0:final]
 
                     texto = re.sub("<.*?>", "", subhtmlText)
-                
-                elif database=="acm":
+
+                elif database == "acm":
                     texto = re.sub("<.*?>", "", htmlText)
 
-                elif database=="ieee":
+                elif database == "ieee":
                     inicio = htmlText.find('"abstract":')
                     subtexto = texto[inicio:]
                     abstractSplit = subtexto.split('"')
-                    if len(abstractSplit)>=3:
+                    if len(abstractSplit) >= 3:
                         texto = abstractSplit[3]
 
-                if texto=="":
+                if texto == "":
                     print("Abstract not found")
                 else:
                     print("Loaded")
-                    entry.fields['abstract'] = texto
+                    entry.fields["abstract"] = texto
 
-                entry.fields['url'] = site
-                if database=="springer":
-                    entry.fields['doi'] = entry.key
+                entry.fields["url"] = site
+                if database == "springer":
+                    entry.fields["doi"] = entry.key
 
                 bibData.entries[entry.key] = entry
                 getAbstract = getAbstract + 1
-                
+
             except:
                 print("url error ", sys.exc_info())
                 urlError = urlError + 1
 
-            
             if getAbstract >= limit:
-                break 
-
+                break
 
     print("")
-    print("Had Abstract ", hadAbstract)
+    print("Had Abstract: ", hadAbstract)
     print("Url errors ", urlError)
-    if (withoutAcmId>0):
-        print("Without ACM id ", withoutAcmId)
+    if withoutAcmId > 0:
+        print("Without ACM id: ", withoutAcmId)
 
-    print("Loaded Abstract", getAbstract)
-    print("Total Entries ", len(bibData.entries))
-    print("Limit to process ", limit)
-    print("Processed ", processed)
-    if (processed<len(bibData.entries)):
-        print("Left ", len(bibData.entries) - processed)
-
+    print("Loaded Abstract: ", getAbstract)
+    print("Total Entries: ", len(bibData.entries))
+    print("Limit to process: ", limit)
+    print("Processed: ", processed)
+    if processed < len(bibData.entries):
+        print("Left: ", len(bibData.entries) - processed)
 
     bibData.to_file(bibFileName)
 
 
-#=============================================================================
+# =============================================================================
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--database", choices=['springer', 'acm', 'ieee'], required=True, help="select database")
+ap.add_argument(
+    "-d",
+    "--database",
+    choices=["springer", "acm", "ieee"],
+    required=True,
+    help="select database",
+)
 ap.add_argument("-f", "--bibFileName", required=True, help="Springer bibFile name")
 
-ap.add_argument("-p", "--proxy", required=False, help="internet proxy, ex: https://john:password123@palmas.pucrs.br:4001")
+ap.add_argument(
+    "-p",
+    "--proxy",
+    required=False,
+    help="internet proxy, ex: https://john:password123@palmas.pucrs.br:4001",
+)
 
-ap.add_argument("-l", "--limit", required=False, help="abstract load limit", default=9999, type=int)
+ap.add_argument(
+    "-l", "--limit", required=False, help="abstract load limit", default=9999, type=int
+)
 
 args = vars(ap.parse_args())
 
 run(args["database"], args["bibFileName"], args["proxy"], args["limit"])
-
-
-
-
-    
-
 
--- SpringerCsv2Bib.py
+++ SpringerCsv2Bib.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, './pybtex/')
+sys.path.insert(0, "./pybtex/")
 from pybtex.database import parse_file, parse_string
 from pybtex.database import BibliographyData, Entry, Person
 
@@ -13,31 +13,32 @@ import unidecode
 from shutil import copyfile
 import tempfile
 
-#=============================================================
+# =============================================================
 def TypePaperSelect(type_tmp):
-    typePaper = 'InProceedings'
-    if (type_tmp=='Article'):
-        typePaper = 'article'
-    elif (type_tmp=='Chapter'):
-        typePaper = 'InProceedings'
+    typePaper = "InProceedings"
+    if type_tmp == "Article":
+        typePaper = "article"
+    elif type_tmp == "Chapter":
+        typePaper = "InProceedings"
     return type_tmp
 
-#=============================================================
+
+# =============================================================
 def AuthorFix(author_tmp):
 
-    #problems with spring CSV
-    
+    # problems with spring CSV
+
     # "Sergey Ablameyko PhD, DSc, Prof, FIEE, FIAPR, SMIEEETony Pridmore BSc, PhD"
     # correct is
     # "Sergey Ablameyko and Tony Pridmore"
-    author_tmp = author_tmp.replace(","," ")
-    author_tmp = author_tmp.replace("PhD","")
-    author_tmp = author_tmp.replace("DSc","")
-    author_tmp = author_tmp.replace("Prof","")
-    author_tmp = author_tmp.replace("FIEE","")
-    author_tmp = author_tmp.replace("FIAPR","")
-    author_tmp = author_tmp.replace("SMIEEE","")
-    author_tmp = author_tmp.replace("  "," ")
+    author_tmp = author_tmp.replace(",", " ")
+    author_tmp = author_tmp.replace("PhD", "")
+    author_tmp = author_tmp.replace("DSc", "")
+    author_tmp = author_tmp.replace("Prof", "")
+    author_tmp = author_tmp.replace("FIEE", "")
+    author_tmp = author_tmp.replace("FIAPR", "")
+    author_tmp = author_tmp.replace("SMIEEE", "")
+    author_tmp = author_tmp.replace("  ", " ")
 
     # "Yingying ZhuCong YaoXiang Bai"
     # correct is
@@ -46,27 +47,38 @@ def AuthorFix(author_tmp):
     author = ""
     for word in author_tmp:
         is_uppercase = word.isupper() and word.isalpha()
-        if (is_uppercase and last_word_isalpha):
+        if is_uppercase and last_word_isalpha:
             author = author + " and "
         author = author + word
         last_word_isalpha = word.islower() and word.isalpha()
 
     return author
 
-#=============================================================
+
+# =============================================================
 def run(csvFileName, bibFileName):
 
     if not os.path.isfile(csvFileName):
-        print("File not found: ",csvFileName)
+        print("File not found: ", csvFileName)
         return
 
     # I dont kown Why, but dont work complex path in Panda, then I copy file to local path
     tmpFile = tempfile.mktemp()
-    copyfile(csvFileName,tmpFile)
+    copyfile(csvFileName, tmpFile)
 
-    colnames = ['title','journal','book','volume','issue','doi','author','year','url','type']
-    pn = pd.read_csv(tmpFile, names=colnames, skiprows=1) 
-
+    colnames = [
+        "title",
+        "journal",
+        "book",
+        "volume",
+        "issue",
+        "doi",
+        "author",
+        "year",
+        "url",
+        "type",
+    ]
+    pn = pd.read_csv(tmpFile, names=colnames, skiprows=1)
 
     bibData = BibliographyData()
     total = 0
@@ -75,41 +87,42 @@ def run(csvFileName, bibFileName):
     for row_index, row in pn.iterrows():
         total = total + 1
         fields = []
-        if (not pd.isnull(row.title)):
-            fields.append(('title', row.title))
-        if (not pd.isnull(row.journal)):
-            fields.append(('journal', row.journal))
-        if (not pd.isnull(row.volume)):
-            fields.append(('volume', str(row.volume)))
-        if (not pd.isnull(row.volume)):
-            fields.append(('issue', str(row.issue)))
-        if (not pd.isnull(row.doi)):
-            fields.append(('doi', row.doi))
-        if (not pd.isnull(row.year)):
-            fields.append(('year', str(row.year)))
-        if (not pd.isnull(row.url)):
-            fields.append(('url', row.url))
-        if (not pd.isnull(row.author)):
-            fields.append(('author', AuthorFix(row.author)))
+        if not pd.isnull(row.title):
+            fields.append(("title", row.title))
+        if not pd.isnull(row.journal):
+            fields.append(("journal", row.journal))
+        if not pd.isnull(row.volume):
+            fields.append(("volume", str(row.volume)))
+        if not pd.isnull(row.volume):
+            fields.append(("issue", str(row.issue)))
+        if not pd.isnull(row.doi):
+            fields.append(("doi", row.doi))
+        if not pd.isnull(row.year):
+            fields.append(("year", str(row.year)))
+        if not pd.isnull(row.url):
+            fields.append(("url", row.url))
+        if not pd.isnull(row.author):
+            fields.append(("author", AuthorFix(row.author)))
 
         keyPaper = row.doi
         typePaper = TypePaperSelect(row.type)
 
-        print("Chave "+keyPaper+"               \r", end="", flush=True)
+        print("Chave " + keyPaper + "               \r", end="", flush=True)
 
-        if (pd.isnull(row.author)):
+        if pd.isnull(row.author):
             notAuthor = notAuthor + 1
         else:
             bibData.entries[keyPaper] = Entry(typePaper, fields)
 
-    print("Processed ",total,"                             ")
-    print("Removed without author ", notAuthor)
-    print("Total Final",len(bibData.entries))
+    print("Processed: ", total, "                             ")
+    print("Removed without author: ", notAuthor)
+    print("Total Final: ", len(bibData.entries))
 
     bibData.to_file(bibFileName)
-    print("Saved file ",bibFileName)
+    print("Saved file: ", bibFileName)
 
-#=============================================================================
+
+# =============================================================================
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--csvFileName", required=True, help="CSV file name")
